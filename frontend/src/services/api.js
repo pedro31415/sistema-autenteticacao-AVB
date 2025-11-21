8c698b30f54c3876f2506d87926c2d3aeeffa14a
Pedro <pedrogoku80@gmail.com>
2025-11-21 00:17:09 -0300

    revise code

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    withCredentials: true,
     headers: {
         "Content-Type": "application/json",
     }
